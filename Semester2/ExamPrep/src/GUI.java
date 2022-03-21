7adfdddae8a65012e17de8dc618696d687f23d1b
stefvnv <stefvnv@yahoo.com>
2022-03-21 20:19:13 +0000

    lab prep

--- Semester2/ExamPrep/src/GUI.java
+++ Semester2/ExamPrep/src/GUI.java
@@ -5,65 +5,71 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 
-public class GUIPart extends JFrame implements ActionListener {
+/*** Question 1
+ * Graphical User Interface
+ * ------------------
+ * Stefana Chiritescu
+ * A00282343
+ */
 
-    private JTabbedPane tabbedPane = new JTabbedPane();
+public class GUI extends JFrame implements ActionListener {
 
+    private JTabbedPane tabbed_pane = new JTabbedPane();
     private JTable table;
+    private JScrollPane scroll_pane;
 
-    private JScrollPane scrollPane;
-
-    private JPanel p1 = new JPanel();
-    private JPanel p2 = new JPanel();
-    private JPanel p3 = new JPanel();
+    private JPanel panel_constituency = new JPanel();
+    private JPanel panel_edit = new JPanel();
+    private JPanel panel_all = new JPanel();
 
     private JComboBox<String> choices;
 
-    private JTextPane textArea = new JTextPane();
+    private JTextPane text_area = new JTextPane();
 
-    private JButton add = new JButton("add");
+    private JButton add = new JButton("Add");
+    private JButton remove = new JButton("Remove");
 
     private ReadCSV csv;
 
-    public GUIPart() {
+    public GUI() {
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setTitle("GUI Part");
+        this.setTitle("Graphical User Interface");
         this.setSize(750, 750);
     }
 
     public void init() {
-        File selectedFile = null;
-        JFileChooser fileChooser = new JFileChooser();
-        fileChooser.setCurrentDirectory(new File("."));
+        File selected_file = null;
+        JFileChooser file_chooser = new JFileChooser();
+        file_chooser.setCurrentDirectory(new File("."));
 
-        int result = fileChooser.showOpenDialog(this.getContentPane());
+        int result = file_chooser.showOpenDialog(this.getContentPane());
+
 
         if (result == JFileChooser.APPROVE_OPTION) {
-            selectedFile = fileChooser.getSelectedFile();
+            selected_file = file_chooser.getSelectedFile();
         }
-
-        csv = new ReadCSV(selectedFile);
+        csv = new ReadCSV(selected_file);
 
         DefaultComboBoxModel<String> options = new DefaultComboBoxModel<>();
         choices = new JComboBox<>(options);
 
 
         for (LocalEleStat stat : csv.getStats()) {
-
             String area = stat.getLocalElectoralArea();
 
             if (options.getIndexOf(area) == -1) {
                 options.addElement(area);
             }
         }
-
         choices.addActionListener(this);
 
-        //___________________________________________
-        // Panel 1
 
-        p1.setLayout(new GridBagLayout());
-
+        /*
+          Panel 1
+          Design a Graphical User Interface program which takes in the “Local Election Dataset” stores it into a data
+          structure (ArrayList) and using a dropdown selection box reveals all Candidates from a chosen constituency
+         */
+        panel_constituency.setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
 
         c.insets = new Insets(0, 5, 0, 5);
@@ -71,20 +77,35 @@ public class GUIPart extends JFrame implements ActionListener {
         c.gridx = 0;
         c.gridy = 0;
 
-        p1.add(choices, c);
+        panel_constituency.add(choices, c);
 
         c.gridy = 1;
-        p1.add(textArea, c);
+        panel_constituency.add(text_area, c);
 
         String area = (String) choices.getSelectedItem();
         setArea(area);
-        //___________________________________________
-        // Panel 2
 
+
+        /*
+          Panel 2
+          The User should be able to add and remove candidates
+         */
+        add.addActionListener(this);
+        panel_edit.add(add);
+
+        panel_edit.add(remove);
+
+
+        /*
+          Panel 3
+          Create a new view (tabbed view) in the GUI to show a list of all Candidates, give the user the ability to sort
+          each of the fields (Remember to use the comparable interface)
+         */
         String[] cols = csv.getHeadings();
 
         DefaultTableModel model = new DefaultTableModel(cols, 0);
         int no = 1;
+
         for (LocalEleStat stat : csv.getStats()) {
             model.addRow(new Object[]{
                     no,
@@ -96,49 +117,39 @@ public class GUIPart extends JFrame implements ActionListener {
             });
             no++;
         }
-
         table = new JTable(model);
-
         table.setAutoCreateRowSorter(true);
+        scroll_pane = new JScrollPane(table);
+        panel_all.add(scroll_pane);
 
-        scrollPane = new JScrollPane(table);
-
-        p2.add(scrollPane);
-
-        //___________________________________________
-        // Panel 3
-
-        add.addActionListener(this);
-        p3.add(add);
+        //--------------------------------------
 
 
-        tabbedPane.add("Select Area", p1);
-        tabbedPane.add("View All", p2);
-        tabbedPane.add("Add New", p3);
+        tabbed_pane.add("Select Area", panel_constituency);
+        tabbed_pane.add("Add/remove candidate", panel_edit);
+        tabbed_pane.add("Listed Candidates", panel_all);
 
-        this.add(tabbedPane);
+        this.add(tabbed_pane);
 
 
         this.setVisible(true);
-
-
     }
 
+
     public void setArea(String area) {
-        textArea.setText(" ");
+        text_area.setText(" ");
         StringBuilder display = new StringBuilder("<html><table>");
         for (LocalEleStat stat : csv.getStats()) {
             if (stat.getLocalElectoralArea().equals(area)) {
                 display.append(stat.toString());
-
             }
-
         }
         display.append("</table></html>");
 
-        textArea.setContentType("text/html");
-        textArea.setText(display.toString());
+        text_area.setContentType("text/html");
+        text_area.setText(display.toString());
     }
+
 
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -149,10 +160,12 @@ public class GUIPart extends JFrame implements ActionListener {
         if (e.getSource() == add) {
             csv.addStat(new LocalEleStat("1,Rock,Noel,\"69 Pinewood Crescent, Glasnevin North, Dublin 9\",Fine Gael,Artane/Whitehall,,,,,"));
         }
-
+        if (e.getSource() == remove){
+            //remove stat
+        }
     }
 
     public static void main(String[] args) {
-        new GUIPart().init();
+        new GUI().init();
     }
 }
---
+++ Semester2/ExamPrep/src/Graphics2D.java
@@ -0,0 +1,9 @@
+/*** Question 2
+ * Graphics 2D
+ * ------------------
+ * Stefana Chiritescu
+ * A00282343
+ */
+
+public class Graphics2D {
+}
